1280042b557ef29b8fc9c23c68877213e0720f73
bpayanpsl <bpayanpsl@gmail.com>
2019-01-21 14:36:33 +0100

    debug

--- src/main/java/nexus/Main.java
+++ src/main/java/nexus/Main.java
@@ -20,8 +20,8 @@ public class Main {
 		} catch (Exception e) {
 			LOG.trace(e.getMessage());
 		} finally {
-			conn1.close();
 			if (conn1 != null) {
+				conn1.close();
 				LOG.trace("Connected with connection #1");
 			}
 		}
