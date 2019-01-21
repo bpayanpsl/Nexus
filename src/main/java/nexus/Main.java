8cd17cc0046f5a62f388aeb46c0b7ea1996e63bb
bpayanpsl <bpayanpsl@gmail.com>
2019-01-21 10:44:05 +0100

    fudfhs

---
+++ src/main/java/nexus/Main.java
@@ -0,0 +1,28 @@
+package nexus;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+	private static Logger LOG = LoggerFactory.getLogger(Main.class);
+
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+		Connection conn1 = null;
+		String dbURL1 = "jdbc:oracle:thin:tiger/scott@localhost:1521:productD8";
+		try {
+			conn1 = DriverManager.getConnection(dbURL1);
+		} catch (Exception e) {
+			LOG.trace(e.getMessage());
+		} finally {
+			if (conn1 != null) {
+				LOG.trace("Connected with connection #1");
+			}
+		}
+	}
+}
