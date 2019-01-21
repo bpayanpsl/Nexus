114d79014840ffe608573bbd228275595bdaa0f6
bpayanpsl <bpayanpsl@gmail.com>
2019-01-21 10:48:39 +0100

    debug

--- src/main/java/nexus/Main.java
+++ src/main/java/nexus/Main.java
@@ -9,7 +9,7 @@ import org.slf4j.LoggerFactory;
 
 public class Main {
 
-	private static Logger LOG = LoggerFactory.getLogger(Main.class);
+	private static final Logger LOG = LoggerFactory.getLogger(Main.class);
 
 	public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
